94473cb011dd23d3213766405ee891acf83d4932
Ben Broshar <benbroshar591@gmail.com>
2022-03-19 01:17:44 -0400

    Added computerPlay function

--- javascript.js
+++ javascript.js
@@ -1,2 +1,21 @@
 // First, setup function computerPlay that chooses R P or S randomly
 
+function computerPlay() {
+    // returns random number from 0 to 3, which is then mapped to either rock, paper, or scissors (1, 2, and 3 respectively).
+    let result = Math.floor(Math.random() * 2.99999999);
+    const compare = result.toString(); 
+
+    if(compare == "0"){
+        let choice = "Rock";
+        return choice;
+    }else if(compare == "1"){
+        let choice = "Paper";
+        return choice;
+    }else if(compare == "2"){
+        let choice = "Scissors";
+        return choice;
+    }else{
+        let choice = "Something went wrong..";
+        return choice;
+    }
+}
